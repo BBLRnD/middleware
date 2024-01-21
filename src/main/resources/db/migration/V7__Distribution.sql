1b0a5b684a4591842f5668cac1a6cfea36fd6242
ahasan34039 <tanvirehsan60@gmail.com>
2024-01-21 10:51:05 +0600

    distribution

---
+++ src/main/resources/db/migration/V7__Distribution.sql
@@ -0,0 +1,6 @@
+INSERT INTO menus
+(id, component, has_children, icon, layer, module, parent_id, `role`, title, `type`)
+VALUES('distributorBillCollections', 'DistributorBillCollectionsComponent', 0, NULL, 2, 'OPERATIONS', 'billPay', 'S_ADMIN', 'Distributor Bill Collections', 'component');
+INSERT INTO menus
+(id, component, has_children, icon, layer, module, parent_id, `role`, title, `type`)
+VALUES('institutionFeePayment', 'InstitutionFeePaymentComponent', 0, NULL, 2, 'OPERATIONS', 'billPay', 'S_ADMIN', 'Institution Fee Payment', 'component');
